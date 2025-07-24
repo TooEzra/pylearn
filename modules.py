6096cd8e21fdb95e40f29f39567ffbe20ef2f85e
TooEzra <ezraktoo09@gmail.com>
2025-07-24 12:49:41 +0300

    python math functions

---
+++ modules.py
@@ -0,0 +1,10 @@
+import math
+
+# Using the math module to calculate the square root
+print(math.sqrt(16))  # Outputs: 4.0
+
+import random
+
+# Generate a random number between 1 and 6 (like rolling a die)
+dice_roll = random.randint(1, 6)
+print("You rolled a", dice_roll)
