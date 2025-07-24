8f1c1d0284b228903499a15fddbab62944c0813b
TooEzra <ezraktoo09@gmail.com>
2025-07-24 12:43:52 +0300

    python functions

---
+++ funtions.py
@@ -0,0 +1,13 @@
+# Defining a function
+def greet(name):
+    print("Hello, " + name + "! 👋")
+
+# Calling the function
+greet("Alice")  # Outputs: Hello, Alice! 👋
+greet("Bob")    # Outputs: Hello, Bob! 👋
+
+def add_numbers(x, y):
+    return x + y
+
+result = add_numbers(10, 5)
+print(result)  # Outputs: 15
